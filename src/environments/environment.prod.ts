5226e45af394847df316e47d2258acce92390ef4
Roosevelt Diaz <roosevelt.diaz@iteria.com.co>
2019-07-15 16:00:17 -0500

    ajuste para el despliegue en servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://127.0.0.1:8000/api/',  
+  BASE_URL: 'http://cloud131.temp.domains/~r79xq8dw/roos/backLaravel/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://127.0.0.1:8000/api/',  
+  BASE_URL: 'http://127.0.0.1:8000/api/'
 };
 
 
